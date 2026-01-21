8f818c77fd0bc72ad4552a854c95601993ab6808
Syed Ali Adan Naqvi <142499778+AliAdanPro@users.noreply.github.com>
2026-01-21 17:48:02 +0500

    ,,
    ,,

--- tables/flashcard_sets.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE flashcard_sets (
-  id SERIAL PRIMARY KEY,
-  document_id INTEGER REFERENCES documents(id) ON DELETE CASCADE,
-  created_at TIMESTAMP DEFAULT NOW()
-);
--- tables/flashcards.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE flashcards (
-  id SERIAL PRIMARY KEY,
-  set_id INTEGER REFERENCES flashcard_sets(id) ON DELETE CASCADE,
-  question TEXT,
-  answer TEXT,
-  difficulty VARCHAR(20) DEFAULT 'EASY'
-);
